e0788cdc0e7a8f03fee2c5747522cc016a11889a
TryFailCryTryAgain <david.k.segerbo@gmail.com>
2025-04-10 21:40:31 +0200

    updated

--- jquery/src/common/script.js
+++ jquery/src/common/script.js
@@ -1,10 +1,68 @@
-$(function() {
-    function BrowseBooksButton() {
-        return $('#browse_books');
-    }
 
-    const $button = BrowseBooksButton();
-    $button?.on('click', function() {
-        console.log("Event has been registered!");
+$(document).ready(function () {
+
+    const $button = $("#browse_books");
+    $button?.on('click', function () {
+        console.log("Event has been registered");
+        window.location.assign("/jquery/src/pages/books.html");
     });
+
+    const url = `https://restful-api-sca9.onrender.com`;
+    const userUrl = 'https://restful-api-sca9.onrender.com/book/';
+
+    function fetchUserData() {
+        console.log("Event has been registered!");
+        $.get(userUrl)
+            .done(function(data) {
+                console.log('Data received:', data);
+                
+                // Check if data is an array
+                if (Array.isArray(data)) {
+                    let genres = data.map(book => book.genre).join(', ');
+                    let titles = data.map(book => book.title).join(', ');
+
+                    let genreList = data.map(book => `<li>${book.genre}, ${book.title}`).join('');
+                    $('#results').append(`<ul>${genreList}</ul>`);
+
+                    // $('#results').append(`Genres: ${genres}`);
+                    // $('#results').append(`Titles: ${titles}`);
+                    
+                    // Alternatively, display in a list format:
+                    // let genreList = data.map(book => `<li>${book.genre}</li>`).join('');
+                    // $('#results').html(`<ul>${genreList}</ul>`);
+                } else {
+                    $('#results').html('<p>No valid book data found</p>');
+                }
+            })
+            .fail(function(error) {
+                console.error('Error:', error);
+                $('#results').html('<p>Error loading data</p>');
+            });
+    }
+    
+    $('#fetch').on('click', fetchUserData);
+
+    function FeaturedBooks() {
+
+        const result = $('#featured_books');
+        $.get(userUrl)
+            .done(function(data) {
+                console.log(`Data Recived: ${data}`);
+
+                if (Array.isArray(data)) {
+                    const limit = 5;
+                    const limitedData = data.slice(0, limit);
+
+                    let bookList = limitedData.map(book => `<div class=book_card><h2>${book.title}</h2><h3>${book.price}</h3></div>`).join('');
+                
+                    $('#results').append(`${bookList}`);
+                } else {
+                    $('#results').html('<p>No valid book data found</p>');
+                }
+            })
+            .fail(function(error) {
+                console.error('Error:', error);
+                $('#results').html('<p>Error loading data</p>');
+            });
+    }
 });
